9365e61eb5c0a7e8ea25bb43baa594bc7f5dcbf2
liniker <linikers@hotmail.com>
2023-06-30 17:08:55 -0300

    hover teste para o card

--- leadster/src/components/card.tsx
+++ leadster/src/components/card.tsx
@@ -16,8 +16,12 @@ const CardVideo = styled.div`
   cursor: pointer;
 
   &:hover {
-    opacity: 0.6;
     color: var(--color-blue-2);
+    /* .image_overlay {
+      opacity: 1;
+      transition: opacity 0.4s ease;
+      position: absolute;
+    } */
   }
 `;
 
@@ -35,6 +39,19 @@ const PlayIcon = styled.div`
   transition: opacity 0.4s ease;
   transform: translate(-50%, -50%);
 `;
+const ImgContainer = styled.div`
+  position: relative;
+`;
+
+const ImgOver = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  background-color: rgba(70, 100, 255, 0.219);
+  transition: opacity 0.4s ease;
+`;
+
 export default function Card() {
   const [isHover, setIsHover] = useState(false);
 
@@ -43,7 +60,11 @@ export default function Card() {
       onMouseEnter={() => setIsHover(true)}
       onMouseLeave={() => setIsHover(false)}
     >
-      <Image src={video} alt="tumb" width={362} height={204} />
+      <ImgContainer>
+        <ImgOver style={{ opacity: isHover ? 1 : 0 }} />
+        <Image src={video} alt="tumb" width={362} height={204} />
+      </ImgContainer>
+
       <TextP>Como aumentar sua geração de Leads feat. Traktor</TextP>
 
       <PlayIcon style={{ opacity: isHover ? 1 : 0 }}>
